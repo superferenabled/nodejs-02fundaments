a2e920bacbd73798fcb4d74b0c1023c04677152f
Fernando Castillo <superferenabled@gmail.com>
2026-09-14 23:43:01 -0600

    feat: update test script to run vitest and enhance test case in app.test.ts chore: enable skipLibCheck in tsconfig.json for improved type checking

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,7 +1,11 @@
-import {describe, test, expect} from 'vitest'
+import {describe, test, expect} from 'vitest';
 
 describe('app.ts', () => {
     test('should be true', () => {
-        expect(true).toBe(true);
+        const num1 =10;
+        const num2 = 20
+        const result = num1 + num2;
+
+        expect(result).toBe(30);
     });
 })
